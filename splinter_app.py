ce001cb372aa70042c2f86fb91f50de12a20493b
urkh <gust989@gmail.com>
2015-06-23 21:54:30 -0430

    adding phantomjs proxy params

--- splinter_app.py
+++ splinter_app.py
@@ -4,9 +4,10 @@ BASE_PATH = os.getcwd()
 
 user_agent = "Mozilla/5.0 (X11; U; Windows x86_128; en-US) AppleWebKit/533.3 (KHTML, like Gecko) Arora/0.11.0 Safari/533.3"
 
-ip = '112.133.213.181' # Indian https proxy 
+ip = '27.34.251.164'
 port = 80
- 
+
+# for firefox driver
 proxy = {
     'network.proxy.type': 1,
     'network.proxy.http': ip,
@@ -18,8 +19,17 @@ proxy = {
     'network.proxy.ftp': ip,
     'network.proxy.ftp_port': port           
 }
- 
-browser = Browser('firefox', user_agent=user_agent, profile_preferences=proxy)
+
+
+# for phantomjs driver
+service_args = [
+    '--proxy='+ip+":"+str(port),
+    '--proxy-type=https',
+] 
+
+#browser = Browser('firefox', user_agent=user_agent, profile_preferences=proxy)
+browser = Browser('phantomjs', user_agent=user_agent, service_args=service_args)
+
 browser.visit('https://duckduckgo.com/?q=ip')
 browser.screenshot(name=BASE_PATH, suffix='_ip.png')
 
